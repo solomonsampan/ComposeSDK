b854aac934319b747ee4b80226f9f0bb57cc2a56
Sampan Solomon <sampan@quintessential.design>
2023-09-12 15:43:51 +0530

    Initial commit

---
+++ Compo/src/main/java/com/example/compo/ShowToast.kt
@@ -0,0 +1,10 @@
+package com.example.compo
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShowToast(){
+
+    Log.i("MyTag", "Show Toast method")
+}
---
+++ app/src/main/java/com/example/sdk/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.sdk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compo.ShowToast
+import com.example.sdk.ui.theme.SDKTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SDKTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShowToast()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SDKTheme {
+        Greeting("Android")
+    }
+}
