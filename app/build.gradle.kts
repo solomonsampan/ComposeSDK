f611e1e95c34758821e85c49a692c161db39ed32
Sampan Solomon <sampan@quintessential.design>
2023-09-13 11:13:38 +0530

    maven publish added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation(project(":Compo"))
+   // implementation(project(":Compo"))
 }
 
--- app/src/main/java/com/example/sdk/MainActivity.kt
+++ app/src/main/java/com/example/sdk/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.compo.ShowToast
 import com.example.sdk.ui.theme.SDKTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShowToast()
+                   // ShowToast()
                 }
             }
         }
