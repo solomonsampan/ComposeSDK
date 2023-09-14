4aea224ba76244da398866ab3330cbb8cfb37cae
Sampan Solomon <sampan@quintessential.design>
2023-09-14 12:54:40 +0530

    Custom text Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-   implementation ("com.github.solomonsampan:ComposeSDK:${rootProject.extra["composesdkVersion1"]}")
+   implementation ("com.github.solomonsampan:ComposeSDK:1.0.32")
     //implementation(project(":Compo"))
 }
 
