af660f6e7a715732a345f15f782679390412f25d
Sampan Solomon <sampan@quintessential.design>
2023-09-27 14:50:18 +0530

    Custom text Added

--- Compo/build.gradle.kts
+++ Compo/build.gradle.kts
@@ -45,7 +45,7 @@ publishing {
         create<MavenPublication>("components") {
             groupId = "com.github.solomonsampan"
             artifactId = "ComposeSDK"
-            version = "1.2.10"
+            version = "1.2.11"
 
             afterEvaluate {
                 artifact(tasks.getByName("bundleReleaseAar"))
