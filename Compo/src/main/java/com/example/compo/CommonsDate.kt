efd9ca11f4ccedb2737683c048bcca3ee115b32e
Sampan Solomon <sampan@quintessential.design>
2023-09-27 15:00:34 +0530

    Custom text Added

---
+++ Compo/src/main/java/com/example/compo/CommonsDate.kt
@@ -0,0 +1,10 @@
+package com.example.compo
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+object CommonsDate {
+
+    val dd_mm_yyyy = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
+
+}
